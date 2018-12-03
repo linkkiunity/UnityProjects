fd83840b54307ac92d1cb8768f00a8a5658a6869
Ryhmatunnus tktlinkki <tktlinkki@lx4-cs-link-01.cs.helsinki.fi>
2018-12-03 19:34:31 +0200

    hyppy toimii

--- PalloKierii/Assets/Scripts/PlayerController.cs
+++ PalloKierii/Assets/Scripts/PlayerController.cs
@@ -1,38 +1,54 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float speed;	
-	private Rigidbody rb;
+    public float speed;
+    private Rigidbody rb;
+    private bool koskeekoLattiaa;
 
-	void Start() 
-	{
-		rb = GetComponent<Rigidbody>();
-	}	
-	void FixedUpdate() 
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        koskeekoLattiaa = true;
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);	
-		rb.AddForce(movement * speed);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
 
 
-		{
-            if (Input.GetKeyDown ("space") ) {
-                Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
-           
-                rb.AddForce (jump);
+        // katsotaan onko space-nappi painettuna
+        if (Input.GetKeyDown("space"))
+        {
+
+            if (koskeekoLattiaa)
+            {
+                //luodaan pystysuuntainen vektori, eli vain y:llä on arvo 
+                Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
+
+                //lisätään vektori pelaaja-hahmolle 
+                rb.AddForce(jump);
+                koskeekoLattiaa = false;
             }
         }
-	}
+    }
 
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag ("PickUp"))
+        // tarkistetaan koskeeko aarretta
+        if (other.gameObject.CompareTag("PickUp"))
         {
-            other.gameObject.SetActive (false);
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.CompareTag("Lattia"))
+        {
+            koskeekoLattiaa = true;
         }
     }
 }
